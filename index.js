3ff01d94703cde10af523dcf2589eb8b53cb5219
MegaArasan <megaarasan2000@gmail.com>
2021-12-24 22:04:49 +0530

    updated

--- index.js
+++ index.js
@@ -9,17 +9,24 @@ const PORT = process.env.PORT;
 app.get("/", (req, res) => {
   res.send("Server Created");
 });
+
+// createfile path to create a filename with current timestamp
 app.get("/createfile", (req, res) => {
   let date = new Date();
+  // replace "/" with "."
+  // toUTCString is a method to get a timestamp
   date = date.toUTCString().replace(/\:/g, ".");
   console.log(date);
+  // creating filename with current date-time.txt
   fs.writeFile(`files/${date}.txt`, date, (err) => {
     console.log("File created successfully");
   });
   res.send({msg:"File created successfully"});
 });
 
+// to get all the files in specific folder
 app.get("/readfile", (req, res) => {
+  // change directory to get the files in folder
   fs.readdir("files", (err, date) => {
     if (err) {
       console.log(err);
